bc155706ed5345f606fd136b76b3ef0491e709b1
Raghu K <raghuk.vit@gmail.com>
2018-04-25 01:24:08 +0000

    Updating httpd

--- httpd/httpd.sh
+++ httpd/httpd.sh
@@ -5,4 +5,5 @@ if [ -z "$TOMCAT_IP" ]; then
     exit 1
 fi
 sed -i -e "/TOMCAT_IP/ c worker.worker1.host=$TOMCAT_IP" /etc/httpd/conf.d/workers.properties
+echo "HOSTNAME = $(hostname) - V2" > /var/www/html/index.html
 /usr/sbin/httpd -DFOREGROUND
